7a6b20d5dedd050b95bf9408c200695dffbe1311
Christopher Medykiewicz <Christopher.Medykiewicz@newrez.com>
2021-10-21 23:11:42 -0400

    Adding scryfall data fetching

---
+++ initCardNameSelector.ts
@@ -0,0 +1,10 @@
+export function initCardNameSelector(userDeckList: any) {
+    let tempCardList = userDeckList.replace(/[0-9]/g, '');
+    let cardList = tempCardList.match(/.+\n/g);
+    cardList.map((cardValues: any) => {
+      cardNames.push({
+        value: cardValues.trim(),
+        viewValue: cardValues.trim()
+      });
+    });
+  }
